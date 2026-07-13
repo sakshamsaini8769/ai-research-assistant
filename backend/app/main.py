e182721a4196c79e7faedc13fb9e28bd3de654d7
Saksham Saini <saksham6598@gmail.ccom>
2026-07-14 01:05:01 +0530

    Fix imports for Render

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,25 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from backend.app.api.routes import router
+from app.api.routes import router
 
-app = FastAPI(
-    title="AI Research Assistant API",
-    version="1.0.0"
-)
+app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://*.vercel.app",
-    ],
+    allow_origins=["*"],   # baad me frontend URL kar denge
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 app.include_router(router)
-
 
 @app.get("/")
 def home():
